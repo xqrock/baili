e8323f2243fdb45d2180d5459dfed8d33b701fdc
xqrock <58764635+xqrock@users.noreply.github.com>
2025-01-26 23:31:37 +0800

    Update diy-part1.sh

--- JDCloud-AX6000-Baili-hanwckf/diy-part1.sh
+++ JDCloud-AX6000-Baili-hanwckf/diy-part1.sh
@@ -13,8 +13,29 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+function merge_package(){
+    repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
+    pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
+    # find package/ -follow -name $pkg -not -path "package/custom/*" | xargs -rt rm -rf
+    git clone --depth=1 --single-branch $1
+    mv $2 package/custom/
+    rm -rf $repo
+}
+function drop_package(){
+    find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -rf
+}
+
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #git clone https://github.com/messense/aliyundrive-webdav package/messense
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
+git clone https://github.com/KFERMercer/luci-app-tcpdump.git package/luci-app-tcpdump
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-alist package/alist
+merge_package https://github.com/kenzok8/openwrt-packages openwrt-packages/luci-app-adguardhome
+merge_package https://github.com/kenzok8/openwrt-packages openwrt-packages/adguardhome
