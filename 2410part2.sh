5cc2bed500612cf0a1c63b9ef24245c8d14bfd85
xqrock <58764635+xqrock@users.noreply.github.com>
2025-02-18 19:44:18 +0800

    Update 2410part2.sh

--- 2410part2.sh
+++ 2410part2.sh
@@ -53,3 +53,15 @@ if [ -f "$SP_FILE" ]; then
 
 	cd $PKG_PATH && echo "ssr-plus has been fixed!"
 fi
+
+#非21.02专用
+if [[ $WRT_BRANCH != *"21"* ]]; then
+	sed -i '/miniupnpd/d; /luci-app-upnp/d' ./.config
+
+	echo "CONFIG_PACKAGE_luci-app-upnp=n" >> ./.config
+	echo "CONFIG_PACKAGE_miniupnpd=n" >> ./.config
+
+	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
+	echo "CONFIG_PACKAGE_luci-app-nikki=y" >> ./.config
+	echo "CONFIG_PACKAGE_luci-app-upnp-mtk-adjust=y" >> ./.config
+fi
