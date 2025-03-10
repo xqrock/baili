152e5122eb8797ec7b152cca501aa82e6659ad7a
xqrock <58764635+xqrock@users.noreply.github.com>
2025-03-10 14:16:28 +0800

    Update diy-part2.sh

--- JDCloud-AX6000-Baili-immortalwrt/diy-part2.sh
+++ JDCloud-AX6000-Baili-immortalwrt/diy-part2.sh
@@ -9,13 +9,20 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-#修复Coremark编译失败
-CM_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/coremark/Makefile")
-if [ -f "$CM_FILE" ]; then
-	sed -i 's/mkdir/mkdir -p/g' $CM_FILE
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+##-----------------Del duplicate packages------------------
+rm -rf feeds/packages/net/open-app-filter
+##-----------------DIY-----------------
+rm -rf ./feeds/packages/net/adguardhome
+rm -rf ./feeds/packages/net/mosdns
+# rm -rf ./feeds/packages/net/shadowsocks-libev
+# rm -rf ./feeds/packages/net/shadowsocks-rust
+# rm -rf ./feeds/packages/net/shadowsocksr-libev
+# rm -rf ./feeds/luci/applications/luci-app-passwall
+# rm -rf ./feeds/luci/applications/luci-app-passwall2
+rm -rf ./feeds/luci/applications/luci-app-ssr-plus
 
-	cd $PKG_PATH && echo "coremark has been fixed!"
-fi
 
 #移除Shadowsocks组件
 PW_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-passwall/Makefile")
@@ -26,24 +33,28 @@ if [ -f "$PW_FILE" ]; then
 
 	cd $PKG_PATH && echo "passwall has been fixed!"
 fi
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-##-----------------Del duplicate packages------------------
-rm -rf feeds/packages/net/open-app-filter
-##-----------------DIY-----------------
-rm -rf ./feeds/packages/net/adguardhome
-rm -rf ./feeds/packages/net/mosdns
-rm -rf ./feeds/packages/net/alist
-# rm -rf ./feeds/packages/net/shadowsocks-libev
-# rm -rf ./feeds/packages/net/shadowsocks-rust
-# rm -rf ./feeds/packages/net/shadowsocksr-libev
-# rm -rf ./feeds/luci/applications/luci-app-passwall
-# rm -rf ./feeds/luci/applications/luci-app-passwall2
-rm -rf ./feeds/luci/applications/luci-app-alist
-rm -rf ./feeds/luci/applications/luci-app-ssr-plus
-rm -rf ./feeds/luci/applications/luci-app-openclash
-rm -rf ./feeds/luci/applications/luci-app-wechatpush
-rm -rf ./feeds/luci/applications/luci-app-ddns-go
-rm -rf ./feeds/luci/applications/luci-app-lucky
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+
+SP_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-ssr-plus/Makefile")
+if [ -f "$SP_FILE" ]; then
+	sed -i '/default PACKAGE_$(PKG_NAME)_INCLUDE_Shadowsocks_Libev/,/libev/d' $SP_FILE
+	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_ShadowsocksR/,/x86_64/d' $SP_FILE
+	sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d; /ShadowsocksR/d' $SP_FILE
+
+	cd $PKG_PATH && echo "ssr-plus has been fixed!"
+fi
+
+#修复TailScale配置文件冲突
+TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Makefile")
+if [ -f "$TS_FILE" ]; then
+	sed -i '/\/files/d' $TS_FILE
+
+	cd $PKG_PATH && echo "tailscale has been fixed!"
+fi
+
+#修复Coremark编译失败
+CM_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/coremark/Makefile")
+if [ -f "$CM_FILE" ]; then
+	sed -i 's/mkdir/mkdir -p/g' $CM_FILE
+
+	cd $PKG_PATH && echo "coremark has been fixed!"
+fi
