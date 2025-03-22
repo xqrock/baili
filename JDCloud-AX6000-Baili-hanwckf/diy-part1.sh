1de56eea29b20ac690a4e48170058fa45d67aefd
xqrock <58764635+xqrock@users.noreply.github.com>
2025-03-22 14:05:24 +0800

    Update diy-part1.sh

--- JDCloud-AX6000-Baili-hanwckf/diy-part1.sh
+++ JDCloud-AX6000-Baili-hanwckf/diy-part1.sh
@@ -29,6 +29,7 @@ function drop_package(){
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git  nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git' >>feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 # git clone https://github.com/immortalwrt/homeproxy.git package/homeproxy
