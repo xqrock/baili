d5c74e92bc9de4699f53bbc24673e3f553f21dd2
xqrock <58764635+xqrock@users.noreply.github.com>
2025-04-10 20:51:17 +0800

    Update padavanonlypart1.sh

--- padavanonlypart1.sh
+++ padavanonlypart1.sh
@@ -31,8 +31,6 @@ function drop_package(){
 git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/OpenWrt-nikki
 git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
 git clone https://github.com/KFERMercer/luci-app-tcpdump.git package/luci-app-tcpdump
-git clone https://github.com/linkease/istore.git package/istore
-git clone https://github.com/linkease/istore-ui.git package/istore-ui
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
